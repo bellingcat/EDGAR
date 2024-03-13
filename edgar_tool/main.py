989eb7bdc88d2d4d868baa3fc84c6480041d4660
msramalho <19508417+msramalho@users.noreply.github.com>
2024-03-13 15:17:42 +0000

    refactoring to poetry and adding pypi workflow
    Co-authored-by: Galen Reich <GalenReich@users.noreply.github.com>

--- edgar_tool/main.py
+++ edgar_tool/main.py
@@ -1,8 +1,8 @@
 from edgar_tool.cli import SecEdgarScraperCli
 import fire
 
-def main():
+def main_entrypoint():
     fire.Fire(SecEdgarScraperCli)
 
 if __name__ == "__main__":
-    main()
+    main_entrypoint()
