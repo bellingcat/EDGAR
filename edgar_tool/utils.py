99ede272aa492c1a21b91468e724691370fe98ff
JackCollins1991 <55454098+JackCollins1991@users.noreply.github.com>
2024-06-08 16:04:39 +0200

    Extend to location full names
    Now values like 'Mexico' are supported instead of just 'O5'

--- edgar_tool/utils.py
+++ edgar_tool/utils.py
@@ -32,9 +32,36 @@ def safe_get(d: Dict, *keys) -> Any:
             return None
     return d
 
-
 def unpack_singleton_list(l: Optional[List]) -> Union[str, List[str]]:
     return l if (l is None) or (len(l) != 1) else l[0]
+
+def invert_dict(d:dict)->dict:
+    """
+        Returns an inverted dictionary such that values are keys and keys are values.
+        If there are duplicate values, the last occurring key-value pair will prevail. 
+    """
+    return {v: k for k, v in d.items()}
+
+def replace_substrings_in_string(s):
+    """
+    Takes a string like "New York, OH" and returns a string with the full 
+    location names converted to codes such as "NY, OH". Returns an unmodified 
+    string if there are no location names present. 
+
+    Parameters:
+    s (str): The original string.
+
+    Returns:
+    str: The modified string with substrings replaced.
+    """
+    locations2codes = invert_dict(TEXT_SEARCH_LOCATIONS_MAPPING)
+    locations2codes = {k.replace(" ", ""): v for k, v in locations2codes.items()}
+    s = s.replace(" ", "")
+    for location in locations2codes.keys():
+        if location in s:
+            s = s.replace(location, locations2codes[location])
+    return s
+    
 
 def parse_location_input(location_input: str | tuple | None) -> str | None:
     """
@@ -42,12 +69,14 @@ def parse_location_input(location_input: str | tuple | None) -> str | None:
 
     This function processes the input to ensure it is in an acceptable format 
     for location searches. Because CLI input like --peo_in "NY, OH" yields
-    python value ('NY','OH'), this function supports single or multiple locations 
+    runtime value ('NY','OH'), this function supports single or multiple locations 
     provided as a string or a tuple. If the input is a tuple, it converts the tuple 
     to a comma-separated string. It also removes any whitespace from the output 
     string to prevent errors during further processing. Also validates that all
     provided location codes are in the TEXT_SEARCH_LOCATIONS_MAPPING and prints
-    the list of acceptable codes if not.
+    the list of acceptable codes if not. If the input string is a location's full name
+    instead of the code (i.e. 'New York' instead of 'NY'), then strings present in
+    TEXT_SEARCH_LOCATIONS_MAPPING.values() are mapped to an code value instead. 
 
     Parameters:
     location_input (str | tuple | None): The input location(s) to be parsed. 
@@ -64,21 +93,21 @@ def parse_location_input(location_input: str | tuple | None) -> str | None:
 
     if not isinstance(location_input, (str, tuple, type(None))):
         raise ValueError(
-            f'peo_in and inc_in must use format like "NY" or "NY,OH,etc" '
+            f'peo_in and inc_in must use format like "NY" or "NY,OH,etc"'
             f'and be one of {TEXT_SEARCH_LOCATIONS_MAPPING}'
         )
-
-    if isinstance(location_input, tuple):
+    if isinstance(location_input,tuple):
+        location_input = ','.join(location_input)
+    
+    if isinstance(location_input,str):
+        ## regardless of input format, convert to tup because --peo_in "New York, OH" is interpreted as a string
+        location_input = tuple(replace_substrings_in_string(location_input).split(','))
         for value in location_input:
-            if value not in TEXT_SEARCH_LOCATIONS_MAPPING:
+            if value not in TEXT_SEARCH_LOCATIONS_MAPPING and value not in TEXT_SEARCH_LOCATIONS_MAPPING.values():
                 raise ValueError(f"{value} not in {TEXT_SEARCH_LOCATIONS_MAPPING}")
         location_input = ','.join(location_input)
 
-    elif isinstance(location_input, str):
-        if location_input not in TEXT_SEARCH_LOCATIONS_MAPPING:
-            raise ValueError(f"{location_input} not in {TEXT_SEARCH_LOCATIONS_MAPPING}")
-
     if location_input:
         location_input = location_input.replace(" ", "")
-
+        
     return location_input
