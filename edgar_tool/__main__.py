c487b7cc88cc775f15ba3c9e657ff0c7e396b725
Jordan Gillard <jordan@blackcicada.info>
2024-12-25 21:16:15 -0500

    feat: Implement remaining CLI args in Typer

--- edgar_tool/__main__.py
+++ edgar_tool/__main__.py
@@ -1,15 +1,16 @@
 from datetime import date, datetime
+from typing import Optional
 
 import typer
 from dateutil.relativedelta import relativedelta
 from typing_extensions import Annotated
 
-from edgar_tool import constants
+from edgar_tool import constants, text_search
 
 app = typer.Typer()
 
 
-@app.command()
+@app.command(no_args_is_help=True)
 def text_search(
     text: Annotated[
         list[str],
@@ -53,7 +54,7 @@ def text_search(
         str,
         typer.Option(help="Company name, ticker, CIK number or individual's name."),
     ] = None,
-    filing_form: Annotated[
+    filing_category: Annotated[
         constants.FilingCategory,
         typer.Option(help="Form group to search for."),
     ] = None,
@@ -65,17 +66,72 @@ def text_search(
             help='List of single forms to search for (e.g. `-sf 10-K -sf "PRE 14A")',
         ),
     ] = None,
-    # # todo: deprecate min_wait and max_wait
-    # min_wait: float = 0.1,
-    # max_wait: float = 0.15,
-    # retries: int = 3,
-    # browser: Optional[str] = None,
-    # headless: Optional[bool] = None,
-    # peo_in: Optional[str] = None,
-    # inc_in: Optional[str] = None,
+    peo_in: Annotated[
+        Optional[str],
+        typer.Option(
+            "--principal-executive-offices-in",
+            "-peoi",
+            help=(
+                "Search for the primary location associated with a filing. "
+                "The principal executive office is where the company's top "
+                "management operates and conducts key business decisions. "
+                "The location could be a US state or territory, a Canadian "
+                "province, or a country."
+            ),
+        ),
+    ] = None,
+    inc_in: Annotated[
+        Optional[str],
+        typer.Option(
+            "--incorporated-in",
+            "-ii",
+            help=(
+                "Search for the primary location associated with a filing. "
+                "Incorporated in refers to the location where the company was "
+                "legally formed and registered as a corporation. "
+                "The location could be a US state or territory, a Canadian "
+                "province, or a country."
+            ),
+        ),
+    ] = None,
+    # todo: deprecate min_wait and max_wait
+    min_wait: Annotated[
+        float,
+        typer.Option(
+            "--min-wait",
+            help="Minimum number of seconds to wait between search",
+        ),
+    ] = 0.1,
+    max_wait: Annotated[
+        float,
+        typer.Option(
+            "--max-wait", help="Maximum number of seconds to wait between search"
+        ),
+    ] = 0.15,
+    retries: Annotated[
+        int,
+        typer.Option(
+            "--retries",
+            "-r",
+            help="Number of times to retry the request before failing",
+        ),
+    ] = 3,
 ):
-    for t in text:
-        print(t)
+    scraper = text_search.EdgarTextSearcher()
+    scraper.text_search(
+        keywords=text,
+        entity_id=entity_id,
+        filing_form=filing_category,
+        single_forms=single_form,
+        start_date=start_date,
+        end_date=end_date,
+        min_wait_seconds=min_wait,
+        max_wait_seconds=max_wait,
+        retries=retries,
+        destination=output,
+        peo_in=peo_in,
+        inc_in=inc_in,
+    )
 
 
 def main():
