f4476fc828dd66da12e6395b1964a613f3670551
Jordan Gillard <jordan@gaproject.com>
2025-01-21 00:46:36 -0500

    style: Add code comments to LocationLiteral

--- edgar_tool/constants.py
+++ edgar_tool/constants.py
@@ -1945,311 +1945,311 @@ FilingCategoryLiteral = Literal[
 DateRangeLiteral = Literal["all", "10y", "5y", "1y", "30d", "custom"]
 
 LocationLiteral = Literal[
-    "AL",
-    "AK",
-    "AZ",
-    "AR",
-    "CA",
-    "CO",
-    "CT",
-    "DE",
-    "DC",
-    "FL",
-    "GA",
-    "HI",
-    "ID",
-    "IL",
-    "IN",
-    "IA",
-    "KS",
-    "KY",
-    "LA",
-    "ME",
-    "MD",
-    "MA",
-    "MI",
-    "MN",
-    "MS",
-    "MO",
-    "MT",
-    "NE",
-    "NV",
-    "NH",
-    "NJ",
-    "NM",
-    "NY",
-    "NC",
-    "ND",
-    "OH",
-    "OK",
-    "OR",
-    "PA",
-    "RI",
-    "SC",
-    "SD",
-    "TN",
-    "TX",
-    "UT",
-    "VT",
-    "VA",
-    "WA",
-    "WV",
-    "WI",
-    "WY",  # US States
-    "AB",
-    "BC",
-    "CAN",
-    "MB",
-    "NB",
-    "NL",
-    "NS",
-    "ON",
-    "PE",
-    "QC",
-    "SK",
-    "YT",  # Canadian Provinces
-    "AFG",
-    "ALA",
-    "ALB",
-    "DZA",
-    "ASM",
-    "AND",
-    "AGO",
-    "AIA",
-    "ATA",
-    "ATG",
-    "ARG",
-    "ARM",
-    "ABW",
-    "AUS",
-    "AUT",
-    "AZE",
-    "BHS",
-    "BHR",
-    "BGD",
-    "BRB",
-    "BLR",
-    "BEL",
-    "BLZ",
-    "BEN",
-    "BMU",
-    "BTN",
-    "BOL",
-    "BIH",
-    "BWA",
-    "BVT",
-    "BRA",
-    "IOT",
-    "BRN",
-    "BGR",
-    "BFA",
-    "BDI",
-    "KHM",
-    "CMR",
-    "CPV",
-    "CYM",
-    "CAF",
-    "TCD",
-    "CHL",
-    "CHN",
-    "CXR",
-    "CCK",
-    "COL",
-    "COM",
-    "COG",
-    "COD",
-    "COK",
-    "CRI",
-    "CIV",
-    "HRV",
-    "CUB",
-    "CYP",
-    "CZE",
-    "DNK",
-    "DJI",
-    "DMA",
-    "DOM",
-    "ECU",
-    "EGY",
-    "SLV",
-    "GNQ",
-    "ERI",
-    "EST",
-    "ETH",
-    "FLK",
-    "FRO",
-    "FJI",
-    "FIN",
-    "FRA",
-    "GUF",
-    "PYF",
-    "ATF",
-    "GAB",
-    "GMB",
-    "GEO",
-    "DEU",
-    "GHA",
-    "GIB",
-    "GRC",
-    "GRL",
-    "GRD",
-    "GLP",
-    "GUM",
-    "GTM",
-    "GGY",
-    "GIN",
-    "GNB",
-    "GUY",
-    "HTI",
-    "HMD",
-    "VAT",
-    "HND",
-    "HKG",
-    "HUN",
-    "ISL",
-    "IND",
-    "IDN",
-    "IRN",
-    "IRQ",
-    "IRL",
-    "IMN",
-    "ISR",
-    "ITA",
-    "JAM",
-    "JPN",
-    "JEY",
-    "JOR",
-    "KAZ",
-    "KEN",
-    "KIR",
-    "PRK",
-    "KOR",
-    "KWT",
-    "KGZ",
-    "LAO",
-    "LVA",
-    "LBN",
-    "LSO",
-    "LBR",
-    "LBY",
-    "LIE",
-    "LTU",
-    "LUX",
-    "MAC",
-    "MKD",
-    "MDG",
-    "MWI",
-    "MYS",
-    "MDV",
-    "MLI",
-    "MLT",
-    "MHL",
-    "MTQ",
-    "MRT",
-    "MUS",
-    "MYT",
-    "MEX",
-    "FSM",
-    "MDA",
-    "MCO",
-    "MNG",
-    "MNE",
-    "MSR",
-    "MAR",
-    "MOZ",
-    "MMR",
-    "NAM",
-    "NRU",
-    "NPL",
-    "NLD",
-    "ANT",
-    "NCL",
-    "NZL",
-    "NIC",
-    "NER",
-    "NGA",
-    "NIU",
-    "NFK",
-    "MNP",
-    "NOR",
-    "OMN",
-    "PAK",
-    "PLW",
-    "PSE",
-    "PAN",
-    "PNG",
-    "PRY",
-    "PER",
-    "PHL",
-    "PCN",
-    "POL",
-    "PRT",
-    "PRI",
-    "QAT",
-    "REU",
-    "ROU",
-    "RUS",
-    "RWA",
-    "BLM",
-    "SHN",
-    "KNA",
-    "LCA",
-    "MAF",
-    "SPM",
-    "VCT",
-    "WSM",
-    "SMR",
-    "STP",
-    "SAU",
-    "SEN",
-    "SRB",
-    "SYC",
-    "SLE",
-    "SGP",
-    "SVK",
-    "SVN",
-    "SLB",
-    "SOM",
-    "ZAF",
-    "SGS",
-    "ESP",
-    "LKA",
-    "SDN",
-    "SUR",
-    "SJM",
-    "SWZ",
-    "SWE",
-    "CHE",
-    "SYR",
-    "TWN",
-    "TJK",
-    "THA",
-    "TLS",
-    "TGO",
-    "TKL",
-    "TON",
-    "TTO",
-    "TUN",
-    "TUR",
-    "TKM",
-    "TCA",
-    "TUV",
-    "UGA",
-    "UKR",
-    "ARE",
-    "GBR",
-    "UMI",
-    "URY",
-    "UZB",
-    "VUT",
-    "VEN",
-    "VNM",
-    "VGB",
-    "VIR",
-    "WLF",
-    "ESH",
-    "YEM",
-    "ZMB",
-    "ZWE",
-    "XX",  # Countries
+    "AL",  # Alabama
+    "AK",  # Alaska
+    "AZ",  # Arizona
+    "AR",  # Arkansas
+    "CA",  # California
+    "CO",  # Colorado
+    "CT",  # Connecticut
+    "DE",  # Delaware
+    "DC",  # District of Columbia
+    "FL",  # Florida
+    "GA",  # Georgia
+    "HI",  # Hawaii
+    "ID",  # Idaho
+    "IL",  # Illinois
+    "IN",  # Indiana
+    "IA",  # Iowa
+    "KS",  # Kansas
+    "KY",  # Kentucky
+    "LA",  # Louisiana
+    "ME",  # Maine
+    "MD",  # Maryland
+    "MA",  # Massachusetts
+    "MI",  # Michigan
+    "MN",  # Minnesota
+    "MS",  # Mississippi
+    "MO",  # Missouri
+    "MT",  # Montana
+    "NE",  # Nebraska
+    "NV",  # Nevada
+    "NH",  # New Hampshire
+    "NJ",  # New Jersey
+    "NM",  # New Mexico
+    "NY",  # New York
+    "NC",  # North Carolina
+    "ND",  # North Dakota
+    "OH",  # Ohio
+    "OK",  # Oklahoma
+    "OR",  # Oregon
+    "PA",  # Pennsylvania
+    "RI",  # Rhode Island
+    "SC",  # South Carolina
+    "SD",  # South Dakota
+    "TN",  # Tennessee
+    "TX",  # Texas
+    "UT",  # Utah
+    "VT",  # Vermont
+    "VA",  # Virginia
+    "WA",  # Washington
+    "WV",  # West Virginia
+    "WI",  # Wisconsin
+    "WY",  # Wyoming
+    "AB",  # Alberta
+    "BC",  # British Columbia
+    "CAN",  # Canada
+    "MB",  # Manitoba
+    "NB",  # New Brunswick
+    "NL",  # Newfoundland and Labrador
+    "NS",  # Nova Scotia
+    "ON",  # Ontario
+    "PE",  # Prince Edward Island
+    "QC",  # Quebec
+    "SK",  # Saskatchewan
+    "YT",  # Yukon
+    "AFG",  # Afghanistan
+    "ALA",  # Åland Islands
+    "ALB",  # Albania
+    "DZA",  # Algeria
+    "ASM",  # American Samoa
+    "AND",  # Andorra
+    "AGO",  # Angola
+    "AIA",  # Anguilla
+    "ATA",  # Antarctica
+    "ATG",  # Antigua and Barbuda
+    "ARG",  # Argentina
+    "ARM",  # Armenia
+    "ABW",  # Aruba
+    "AUS",  # Australia
+    "AUT",  # Austria
+    "AZE",  # Azerbaijan
+    "BHS",  # Bahamas
+    "BHR",  # Bahrain
+    "BGD",  # Bangladesh
+    "BRB",  # Barbados
+    "BLR",  # Belarus
+    "BEL",  # Belgium
+    "BLZ",  # Belize
+    "BEN",  # Benin
+    "BMU",  # Bermuda
+    "BTN",  # Bhutan
+    "BOL",  # Bolivia
+    "BIH",  # Bosnia and Herzegovina
+    "BWA",  # Botswana
+    "BVT",  # Bouvet Island
+    "BRA",  # Brazil
+    "IOT",  # British Indian Ocean Territory
+    "BRN",  # Brunei Darussalam
+    "BGR",  # Bulgaria
+    "BFA",  # Burkina Faso
+    "BDI",  # Burundi
+    "KHM",  # Cambodia
+    "CMR",  # Cameroon
+    "CPV",  # Cape Verde
+    "CYM",  # Cayman Islands
+    "CAF",  # Central African Republic
+    "TCD",  # Chad
+    "CHL",  # Chile
+    "CHN",  # China
+    "CXR",  # Christmas Island
+    "CCK",  # Cocos (Keeling) Islands
+    "COL",  # Colombia
+    "COM",  # Comoros
+    "COG",  # Congo
+    "COD",  # Congo, the Democratic Republic of the
+    "COK",  # Cook Islands
+    "CRI",  # Costa Rica
+    "CIV",  # Côte d'Ivoire
+    "HRV",  # Croatia
+    "CUB",  # Cuba
+    "CYP",  # Cyprus
+    "CZE",  # Czech Republic
+    "DNK",  # Denmark
+    "DJI",  # Djibouti
+    "DMA",  # Dominica
+    "DOM",  # Dominican Republic
+    "ECU",  # Ecuador
+    "EGY",  # Egypt
+    "SLV",  # El Salvador
+    "GNQ",  # Equatorial Guinea
+    "ERI",  # Eritrea
+    "EST",  # Estonia
+    "ETH",  # Ethiopia
+    "FLK",  # Falkland Islands (Malvinas)
+    "FRO",  # Faroe Islands
+    "FJI",  # Fiji
+    "FIN",  # Finland
+    "FRA",  # France
+    "GUF",  # French Guiana
+    "PYF",  # French Polynesia
+    "ATF",  # French Southern Territories
+    "GAB",  # Gabon
+    "GMB",  # Gambia
+    "GEO",  # Georgia
+    "DEU",  # Germany
+    "GHA",  # Ghana
+    "GIB",  # Gibraltar
+    "GRC",  # Greece
+    "GRL",  # Greenland
+    "GRD",  # Grenada
+    "GLP",  # Guadeloupe
+    "GUM",  # Guam
+    "GTM",  # Guatemala
+    "GGY",  # Guernsey
+    "GIN",  # Guinea
+    "GNB",  # Guinea-Bissau
+    "GUY",  # Guyana
+    "HTI",  # Haiti
+    "HMD",  # Heard Island and McDonald Islands
+    "VAT",  # Holy See (Vatican City State)
+    "HND",  # Honduras
+    "HKG",  # Hong Kong
+    "HUN",  # Hungary
+    "ISL",  # Iceland
+    "IND",  # India
+    "IDN",  # Indonesia
+    "IRN",  # Iran, Islamic Republic of
+    "IRQ",  # Iraq
+    "IRL",  # Ireland
+    "IMN",  # Isle of Man
+    "ISR",  # Israel
+    "ITA",  # Italy
+    "JAM",  # Jamaica
+    "JPN",  # Japan
+    "JEY",  # Jersey
+    "JOR",  # Jordan
+    "KAZ",  # Kazakhstan
+    "KEN",  # Kenya
+    "KIR",  # Kiribati
+    "PRK",  # Korea, Democratic People's Republic of
+    "KOR",  # Korea, Republic of
+    "KWT",  # Kuwait
+    "KGZ",  # Kyrgyzstan
+    "LAO",  # Lao People's Democratic Republic
+    "LVA",  # Latvia
+    "LBN",  # Lebanon
+    "LSO",  # Lesotho
+    "LBR",  # Liberia
+    "LBY",  # Libya
+    "LIE",  # Liechtenstein
+    "LTU",  # Lithuania
+    "LUX",  # Luxembourg
+    "MAC",  # Macao
+    "MKD",  # Macedonia, the former Yugoslav Republic of
+    "MDG",  # Madagascar
+    "MWI",  # Malawi
+    "MYS",  # Malaysia
+    "MDV",  # Maldives
+    "MLI",  # Mali
+    "MLT",  # Malta
+    "MHL",  # Marshall Islands
+    "MTQ",  # Martinique
+    "MRT",  # Mauritania
+    "MUS",  # Mauritius
+    "MYT",  # Mayotte
+    "MEX",  # Mexico
+    "FSM",  # Micronesia, Federated States of
+    "MDA",  # Moldova, Republic of
+    "MCO",  # Monaco
+    "MNG",  # Mongolia
+    "MNE",  # Montenegro
+    "MSR",  # Montserrat
+    "MAR",  # Morocco
+    "MOZ",  # Mozambique
+    "MMR",  # Myanmar
+    "NAM",  # Namibia
+    "NRU",  # Nauru
+    "NPL",  # Nepal
+    "NLD",  # Netherlands
+    "ANT",  # Netherlands Antilles
+    "NCL",  # New Caledonia
+    "NZL",  # New Zealand
+    "NIC",  # Nicaragua
+    "NER",  # Niger
+    "NGA",  # Nigeria
+    "NIU",  # Niue
+    "NFK",  # Norfolk Island
+    "MNP",  # Northern Mariana Islands
+    "NOR",  # Norway
+    "OMN",  # Oman
+    "PAK",  # Pakistan
+    "PLW",  # Palau
+    "PSE",  # Palestine, State of
+    "PAN",  # Panama
+    "PNG",  # Papua New Guinea
+    "PRY",  # Paraguay
+    "PER",  # Peru
+    "PHL",  # Philippines
+    "PCN",  # Pitcairn
+    "POL",  # Poland
+    "PRT",  # Portugal
+    "PRI",  # Puerto Rico
+    "QAT",  # Qatar
+    "REU",  # Réunion
+    "ROU",  # Romania
+    "RUS",  # Russian Federation
+    "RWA",  # Rwanda
+    "BLM",  # Saint Barthélemy
+    "SHN",  # Saint Helena, Ascension and Tristan da Cunha
+    "KNA",  # Saint Kitts and Nevis
+    "LCA",  # Saint Lucia
+    "MAF",  # Saint Martin (French part)
+    "SPM",  # Saint Pierre and Miquelon
+    "VCT",  # Saint Vincent and the Grenadines
+    "WSM",  # Samoa
+    "SMR",  # San Marino
+    "STP",  # Sao Tome and Principe
+    "SAU",  # Saudi Arabia
+    "SEN",  # Senegal
+    "SRB",  # Serbia
+    "SYC",  # Seychelles
+    "SLE",  # Sierra Leone
+    "SGP",  # Singapore
+    "SVK",  # Slovakia
+    "SVN",  # Slovenia
+    "SLB",  # Solomon Islands
+    "SOM",  # Somalia
+    "ZAF",  # South Africa
+    "SGS",  # South Georgia and the South Sandwich Islands
+    "ESP",  # Spain
+    "LKA",  # Sri Lanka
+    "SDN",  # Sudan
+    "SUR",  # Suriname
+    "SJM",  # Svalbard and Jan Mayen
+    "SWZ",  # Swaziland
+    "SWE",  # Sweden
+    "CHE",  # Switzerland
+    "SYR",  # Syrian Arab Republic
+    "TWN",  # Taiwan, Province of China
+    "TJK",  # Tajikistan
+    "THA",  # Thailand
+    "TLS",  # Timor-Leste
+    "TGO",  # Togo
+    "TKL",  # Tokelau
+    "TON",  # Tonga
+    "TTO",  # Trinidad and Tobago
+    "TUN",  # Tunisia
+    "TUR",  # Turkey
+    "TKM",  # Turkmenistan
+    "TCA",  # Turks and Caicos Islands
+    "TUV",  # Tuvalu
+    "UGA",  # Uganda
+    "UKR",  # Ukraine
+    "ARE",  # United Arab Emirates
+    "GBR",  # United Kingdom
+    "UMI",  # United States Minor Outlying Islands
+    "URY",  # Uruguay
+    "UZB",  # Uzbekistan
+    "VUT",  # Vanuatu
+    "VEN",  # Venezuela, Bolivarian Republic of
+    "VNM",  # Viet Nam
+    "VGB",  # Virgin Islands, British
+    "VIR",  # Virgin Islands, U.S.
+    "WLF",  # Wallis and Futuna
+    "ESH",  # Western Sahara
+    "YEM",  # Yemen
+    "ZMB",  # Zambia
+    "ZWE",  # Zimbabwe
+    "XX",  # Unknown
 ]
