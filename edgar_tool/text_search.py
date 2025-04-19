a4c45a5a89cd794fcaad845dce0c0afb9d276763
Jordan Gillard <jordan@gaproject.com>
2025-04-19 00:24:58 -0400

    remove page_fetcher.py

--- edgar_tool/text_search.py
+++ edgar_tool/text_search.py
@@ -13,7 +13,6 @@ from edgar_tool.constants import (
     TEXT_SEARCH_LOCATIONS_MAPPING,
 )
 from edgar_tool.io import write_results_to_file
-from edgar_tool.page_fetcher import PageCheckFailedError
 from edgar_tool.url_generator import SearchParams, generate_search_url_for_kwargs
 from edgar_tool.utils import split_date_range_in_half, unpack_singleton_list
 
@@ -181,6 +180,10 @@ def search(
             output,
             TEXT_SEARCH_CSV_FIELDS_NAMES,
         )
+
+
+class PageCheckFailedError(Exception):
+    pass
 
 
 @retry(
