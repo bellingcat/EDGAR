d0dbf9852b110781452110c38b8c0e9031eb0775
Jordan Gillard <jordan@gaproject.com>
2025-05-13 23:33:32 -0400

    Fix broken jupyter notebook

--- src/edgar_tool/__init__.py
+++ src/edgar_tool/__init__.py
@@ -1,3 +1,5 @@
 from edgar_tool.cli import app
+from edgar_tool.search_params import SearchParams
+from edgar_tool.text_search import search
 
-__all__ = ["app"]
+__all__ = ["app", "SearchParams", "search"]
