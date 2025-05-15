46cacf052304e3f0e46b6998051518b38f2f4a43
Jordan Gillard <jordan@gaproject.com>
2025-05-14 21:05:07 -0400

    refactor: Location options use official names
    Updates the location form field in the Google Colab notebook to use
    proper location names (like Alaska vs AK).

--- src/edgar_tool/__init__.py
+++ src/edgar_tool/__init__.py
@@ -1,5 +1,6 @@
 from edgar_tool.cli import app
+from edgar_tool.constants import Location
 from edgar_tool.search_params import SearchParams
 from edgar_tool.text_search import search
 
-__all__ = ["app", "SearchParams", "search"]
+__all__ = ["app", "SearchParams", "search", "Location"]
